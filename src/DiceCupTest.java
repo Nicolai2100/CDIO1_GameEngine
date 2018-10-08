395688e302d393fd2add3b57b445e17255452f36
Nicolai Nisbeth <nicolai.nisbeth@hotmail.com>
2018-10-08 17:51:44 +0200

    1. Commit - Oprettet JUnitTest til alle klasser

---
+++ src/DiceCupTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DiceCupTest {
+
+    @Test
+    public void rollSum() {
+    }
+}
---
+++ src/PlayerTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void playerRoll() {
+    }
+
+    @Test
+    public void playerWon() {
+    }
+
+    @Test
+    public void playerGotTwoOfEqualValue() {
+    }
+
+    @Test
+    public void playerWonBydoubleSix() {
+    }
+
+    @Test
+    public void decider() {
+    }
+}
