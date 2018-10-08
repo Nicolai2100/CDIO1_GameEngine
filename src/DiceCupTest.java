bda498f708e4930fb1940520585e14bb0de1be76
Nicolai Nisbeth <nicolai.nisbeth@hotmail.com>
2018-10-08 19:33:07 +0200

    5. Commit - tilføjet yderlige tests til DiceCupTest og DiceTest

--- src/DiceCupTest.java
+++ src/DiceCupTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static org.junit.Assert.*;
 
 public class DiceCupTest {
@@ -10,5 +13,35 @@ public class DiceCupTest {
     public void rollSum() {
         int sum = diceCup1.rollSum();
         assertEquals((die1.getFaceValue() + die2.getFaceValue()),sum);
+
+        for (int j = 1; j<=1000; j++) {
+            int øjneTerning1 = die1.roll();
+            int øjneTerning2 = die2.roll();
+            int øjneSum = øjneTerning1 + øjneTerning2;
+            assertTrue(2 <= øjneSum && øjneSum <= 12);
+        }
+
+        // tester om sum af 2 terninger fordeler sig ud fra
+        // den teoretiske sandsynlighed. Afvigelse mindre end 2 %
+        // accepteres. På 10000 kast svarer det til +- 200 kast.
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int i = 1; i <= 10000; i++) {
+            list.add(die1.roll() + die2.roll());
+        }
+
+        // https://wizardofodds.com/gambling/dice/
+        // &&
+        // http://alumnus.caltech.edu/~leif/FRP/probability.html
+        double[] teoretiskSandsynlighed = {2.78, 5.56, 8.33, 11.11, 13.89, 16.67,
+                13.89, 11.11, 8.33, 5.56, 2.78};
+        int index = 0;
+        for (int i = 2; i <= 12; i++) {
+            double tilfældeAfØjne = Collections.frequency(list, i);
+            double tilfældeIProcent = (tilfældeAfØjne / 10000) * 100;
+            double afvigelse = Math.abs(tilfældeIProcent - teoretiskSandsynlighed[index++]);
+            assertTrue( afvigelse <= 2.0);
+        }
     }
+
+
 }
--- src/DiceTest.java
+++ src/DiceTest.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 import static org.junit.Assert.*;
 
 public class DiceTest {
@@ -8,6 +11,18 @@ public class DiceTest {
         for (int i = 1; i<=1000; i++){
             int faceValue = die.roll();
             assertTrue(1<= faceValue && faceValue <= 6);
+        }
+
+        // Vi tester om terningen over 60000 kast fordeler sig statistisk repræsentativt.
+        // Det vil sige at alle øjne skal have været vist 10000 gange plus/minus 400.
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for (int j = 1; j <= 60000; j++){
+            list.add(die.roll());
+        }
+
+        for (int j = 1; j<=6; j++){
+            int test = Collections.frequency(list, j);
+            assertTrue(9600 <= test && test <= 10400);
         }
     }
 }
