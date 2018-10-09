e76222da420f233da80df1c0953e3550798b9da8
Nicolai Nisbeth <nicolai.nisbeth@hotmail.com>
2018-10-09 08:02:13 +0200

    6. Commit - rettet i diceTest og diceCupTest

--- src/DiceCupTest.java
+++ src/DiceCupTest.java
@@ -12,36 +12,35 @@ public class DiceCupTest {
     @Test
     public void rollSum() {
         int sum = diceCup1.rollSum();
-        assertEquals((die1.getFaceValue() + die2.getFaceValue()),sum);
+        assertEquals((die1.getFaceValue() + die2.getFaceValue()), sum);
 
         for (int j = 1; j<=1000; j++) {
-            int øjneTerning1 = die1.roll();
-            int øjneTerning2 = die2.roll();
-            int øjneSum = øjneTerning1 + øjneTerning2;
-            assertTrue(2 <= øjneSum && øjneSum <= 12);
+            int faceValue1 = die1.roll();
+            int faceValue2 = die2.roll();
+            int faceValueSum = faceValue1 + faceValue2;
+            assertTrue(2 <= faceValueSum && faceValueSum <= 12);
         }
 
-        // tester om sum af 2 terninger fordeler sig ud fra
-        // den teoretiske sandsynlighed. Afvigelse mindre end 2 %
-        // accepteres. På 10000 kast svarer det til +- 200 kast.
+        // we test if the sum of two die is according
+        // to the theoretical probability over 10000 rolls. Deviation less than 2%
+        // is accepted.
         ArrayList<Integer> list = new ArrayList<Integer>();
         for (int i = 1; i <= 10000; i++) {
             list.add(die1.roll() + die2.roll());
         }
 
-        // https://wizardofodds.com/gambling/dice/
-        // &&
-        // http://alumnus.caltech.edu/~leif/FRP/probability.html
-        double[] teoretiskSandsynlighed = {2.78, 5.56, 8.33, 11.11, 13.89, 16.67,
-                13.89, 11.11, 8.33, 5.56, 2.78};
+        // Source: https://wizardofodds.com/gambling/dice/
+        double[] theoreticalProbability = {
+                                            2.78, 5.56, 8.33, 11.11, 13.89, 16.67,
+                                            13.89, 11.11, 8.33, 5.56, 2.78
+                                          };
+
         int index = 0;
         for (int i = 2; i <= 12; i++) {
-            double tilfældeAfØjne = Collections.frequency(list, i);
-            double tilfældeIProcent = (tilfældeAfØjne / 10000) * 100;
-            double afvigelse = Math.abs(tilfældeIProcent - teoretiskSandsynlighed[index++]);
-            assertTrue( afvigelse <= 2.0);
+            double faceValueInstance = Collections.frequency(list, i);
+            double instanceInPercent = (faceValueInstance / 10000) * 100;
+            double deviation = Math.abs(instanceInPercent - theoreticalProbability[index++]);
+            assertTrue( deviation <= 2.0);
         }
     }
-
-
 }
