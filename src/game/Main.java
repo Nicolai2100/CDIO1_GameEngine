e564f694bc426d199ab014537283946ca3436802
Nicolai <nicolailarsen2100@gmail.com>
2018-10-22 16:02:52 +0200

    Koncerteret projektet til et Maven projekt

--- src/game/Main.java
+++
@@ -1,8 +0,0 @@
-package game;//import static com.sun.tools.doclint.Entity.or;
-
-public class Main {
-    public static void main(String[] args) {
-        GameEngine gameEngine = new GameEngine();
-        gameEngine.playGame();
-    }
-}
